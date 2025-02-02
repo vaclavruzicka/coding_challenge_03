499c1a55bfc52d6dad67fd0a3b6f7cfcc2f5379c
Vaclav Ruzicka <vaclavruzicka@usf.edu>
2025-02-02 16:14:25 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,13 @@
+//Task 1 - Product Price Managment Scenario
+
+//Declaring an array with five numerical values.
+let prices = [37.99, 115.99, 121.99, 124.99, 129.99]
+
+//Add a new price to the array.
+prices.push(134.99)
+
+//Remove the first price from the array.
+prices.shift()
+
+//Log the updated price list.
+console.log(prices)
