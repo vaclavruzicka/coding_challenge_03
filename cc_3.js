bd0e46fd2c1e65dccc4a7576122e4ee205680fe4
Vaclav Ruzicka <vaclavruzicka@usf.edu>
2025-02-02 16:28:11 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -11,3 +11,18 @@ prices.shift()
 
 //Log the updated price list.
 console.log(prices)
+
+//Task 2 - Modifying Customer Orders Scenario
+
+//Declaring an array.
+let orders = [12, 15, 18, 23, 30]
+
+//Modifying the third value in the array.
+orders.splice(2, 2, 19, 23);
+
+//Calculating the sum of the array.
+let total_orders = orders.reduce((sum, order) => sum + order, 0)
+
+//Log the updated array and the total of the array
+console.log("Orders:", orders)
+console.log("Sum of Orders:", total_orders)
